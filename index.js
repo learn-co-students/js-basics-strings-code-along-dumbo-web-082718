b09e3480dc5eb2bb47fddd702ab92f5963694dc3
tiffanynuugen <tiffanynuugen@gmail.com>
2018-10-29 12:36:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'tiff';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//const shortGreeting = 'Welcome, ';
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
